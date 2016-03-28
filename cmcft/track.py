5b149af1c3f2e79b61c4c8db8f061d19f81f61ac
Alan Aberdeen <alan@Fly.local>
2016-03-28 15:20:10 +0100

    Updates to packages
    Updating a few requirements

--- cmcft/track.py
+++ cmcft/track.py
@@ -88,12 +88,12 @@ parent = os.path.abspath(os.path.join(cwd, '../../'))
 # --------
 # Quick test path variables for comparison
 # Test1
-images = parent + '/datasets/Fluo-N2DH-SIM/01_GT/SEG'
+#images = parent + '/datasets/Fluo-N2DH-SIM/01_GT/SEG'
 # Test2
-#images = parent + '/datasets/PhC-C2DH-U373/01_GT/SEG'
+images = parent + '/test_datasets/PhC-C2DH-U373/01_GT/SEG'
 
 # Setup save area
-save = parent + '/datasets/test_results'
+save = parent + '/test_datasets/test_results'
 previous_saves = glob.glob(save + '/*')
 for f in previous_saves:
     os.remove(f)
